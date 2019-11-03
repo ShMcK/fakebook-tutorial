1d55cc553d4bcef3e603c804999d1458d8cf8afa
shmck <shawn.j.mckay@gmail.com>
2019-11-02 21:02:43 -0700

    setup: start express server

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -2,8 +2,26 @@ import { ApolloServer } from 'apollo-server'
 import { server } from '../src/main'
 
 describe('Apollo Server', () => {
+	afterAll(() => {
+		// @ts-ignore // accessing private internals
+		if (server.httpServer) {
+			// close the port connection to avoid 
+			// "port in use" errors on retries
+			// @ts-ignore // accessing private internals
+			server.httpServer.close()
+		}
+	})
+
 	test('should be created', () => {
 		expect(server).toBeDefined()
 		expect(server).toBeInstanceOf(ApolloServer)
 	})
+
+	test('starts Express server', () => {
+		// ApolloServer attaches an Express
+		// httpServer when "listen" is called. 
+		// @ts-ignore // accessing private internals
+		expect(server.httpServer).toBeDefined()
+	})
+
 })
