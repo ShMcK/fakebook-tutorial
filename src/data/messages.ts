ea02c3032b00cd789c83fbcfbd3adbe990b1daef
shmck <shawn.j.mckay@gmail.com>
2019-11-03 18:15:02 -0800

    level setup: messages

---
+++ src/data/messages.ts
@@ -0,0 +1,607 @@
+const messages = [
+
+  {
+    id: '1',
+    createdAt: '2019-11-04T02:09:02.823Z',
+    user: '29',
+    content: 'Ut ut aut.'
+  },
+  {
+    id: '2',
+    createdAt: '2019-11-03T04:59:24.268Z',
+    user: '7',
+    content: 'Est tenetur eius suscipit consequatur ab.'
+  },
+  {
+    id: '3',
+    createdAt: '2019-11-03T06:17:45.402Z',
+    user: '17',
+    content: 'Deserunt a et quae laboriosam eveniet.'
+  },
+  {
+    id: '4',
+    createdAt: '2019-11-03T15:40:45.687Z',
+    user: '10',
+    content: 'Similique quis provident aut.'
+  },
+  {
+    id: '5',
+    createdAt: '2019-11-03T07:40:57.197Z',
+    user: '27',
+    content: 'Modi qui nisi qui qui.'
+  },
+  {
+    id: '6',
+    createdAt: '2019-11-03T13:30:34.443Z',
+    user: '16',
+    content: 'Ratione dignissimos consequatur aut magni omnis placeat assumenda.'
+  },
+  {
+    id: '7',
+    createdAt: '2019-11-03T06:52:02.123Z',
+    user: '15',
+    content: 'Eum odio eligendi et est saepe est non.'
+  },
+  {
+    id: '8',
+    createdAt: '2019-11-03T08:31:31.635Z',
+    user: '2',
+    content: 'Suscipit saepe id rem consequatur qui distinctio aliquam.'
+  },
+  {
+    id: '9',
+    createdAt: '2019-11-03T19:31:08.442Z',
+    user: '14',
+    content: 'Corporis autem ut dolor et possimus nihil optio.'
+  },
+  {
+    id: '10',
+    createdAt: '2019-11-03T08:41:05.415Z',
+    user: '30',
+    content: 'Quae suscipit exercitationem perferendis.'
+  },
+  {
+    id: '11',
+    createdAt: '2019-11-03T09:36:49.054Z',
+    user: '27',
+    content: 'Illum qui quas pariatur non.'
+  },
+  {
+    id: '12',
+    createdAt: '2019-11-03T02:39:41.064Z',
+    user: '27',
+    content: 'Molestiae placeat deleniti voluptatem minus tenetur.'
+  },
+  {
+    id: '13',
+    createdAt: '2019-11-03T03:09:12.170Z',
+    user: '5',
+    content: 'Reiciendis consequatur quas ea maiores est id.'
+  },
+  {
+    id: '14',
+    createdAt: '2019-11-03T19:13:42.905Z',
+    user: '12',
+    content: 'Similique quo quidem et et quaerat ad excepturi incidunt.'
+  },
+  {
+    id: '15',
+    createdAt: '2019-11-03T19:50:58.081Z',
+    user: '8',
+    content: 'Nihil omnis molestiae voluptatum et modi voluptas aliquid.'
+  },
+  {
+    id: '16',
+    createdAt: '2019-11-03T14:24:25.438Z',
+    user: '22',
+    content: 'Dignissimos placeat voluptate quis quisquam est nam veritatis necessitatibus quia.'
+  },
+  {
+    id: '17',
+    createdAt: '2019-11-03T20:00:01.566Z',
+    user: '23',
+    content: 'Deserunt quia et tempore eaque voluptatem consequatur.'
+  },
+  {
+    id: '18',
+    createdAt: '2019-11-03T21:38:02.191Z',
+    user: '18',
+    content: 'Quisquam dolor et quia similique repellat laudantium in non.'
+  },
+  {
+    id: '19',
+    createdAt: '2019-11-03T16:26:54.297Z',
+    user: '21',
+    content: 'Et velit optio provident officia dolor id esse.'
+  },
+  {
+    id: '20',
+    createdAt: '2019-11-03T13:48:45.589Z',
+    user: '1',
+    content: 'Et similique quibusdam.'
+  },
+  {
+    id: '21',
+    createdAt: '2019-11-03T05:37:35.740Z',
+    user: '30',
+    content: 'Esse nisi beatae.'
+  },
+  {
+    id: '22',
+    createdAt: '2019-11-03T11:58:57.116Z',
+    user: '30',
+    content: 'Molestias voluptate nemo voluptatem molestiae omnis commodi rerum ab.'
+  },
+  {
+    id: '23',
+    createdAt: '2019-11-04T01:05:40.282Z',
+    user: '21',
+    content: 'Explicabo asperiores eaque fugit eveniet.'
+  },
+  {
+    id: '24',
+    createdAt: '2019-11-03T16:27:05.185Z',
+    user: '5',
+    content: 'Asperiores iste molestiae esse in.'
+  },
+  {
+    id: '25',
+    createdAt: '2019-11-03T22:31:36.874Z',
+    user: '21',
+    content: 'Id quibusdam sit voluptate.'
+  },
+  {
+    id: '26',
+    createdAt: '2019-11-03T04:46:22.756Z',
+    user: '9',
+    content: 'Voluptatem voluptate corporis asperiores.'
+  },
+  {
+    id: '27',
+    createdAt: '2019-11-03T06:29:55.046Z',
+    user: '18',
+    content: 'Debitis est odit nihil voluptate consequatur voluptas totam.'
+  },
+  {
+    id: '28',
+    createdAt: '2019-11-03T22:55:00.236Z',
+    user: '24',
+    content: 'Reprehenderit est eveniet vitae esse voluptates et.'
+  },
+  {
+    id: '29',
+    createdAt: '2019-11-04T00:33:18.813Z',
+    user: '18',
+    content: 'Praesentium aliquam itaque quis similique quae qui accusantium qui.'
+  },
+  {
+    id: '30',
+    createdAt: '2019-11-04T01:06:34.793Z',
+    user: '29',
+    content: 'Ullam ipsum et aut sint earum a.'
+  },
+  {
+    id: '31',
+    createdAt: '2019-11-03T14:32:33.789Z',
+    user: '13',
+    content: 'Mollitia quia optio rerum aliquam enim.'
+  },
+  {
+    id: '32',
+    createdAt: '2019-11-03T07:02:25.610Z',
+    user: '27',
+    content: 'Voluptas eos ex totam doloremque ut sapiente atque aut.'
+  },
+  {
+    id: '33',
+    createdAt: '2019-11-03T15:14:37.748Z',
+    user: '23',
+    content: 'Eius tenetur cum in est in.'
+  },
+  {
+    id: '34',
+    createdAt: '2019-11-04T01:15:47.279Z',
+    user: '6',
+    content: 'Et perspiciatis porro ipsum perferendis quia molestiae ullam.'
+  },
+  {
+    id: '35',
+    createdAt: '2019-11-04T01:41:21.401Z',
+    user: '11',
+    content: 'Hic sint maiores.'
+  },
+  {
+    id: '36',
+    createdAt: '2019-11-03T22:46:09.482Z',
+    user: '10',
+    content: 'Ducimus iste quo eligendi molestias.'
+  },
+  {
+    id: '37',
+    createdAt: '2019-11-03T10:01:54.713Z',
+    user: '6',
+    content: 'Quo enim cum fuga iste sed.'
+  },
+  {
+    id: '38',
+    createdAt: '2019-11-03T13:41:23.908Z',
+    user: '1',
+    content: 'Mollitia nihil ut illum.'
+  },
+  {
+    id: '39',
+    createdAt: '2019-11-03T04:21:29.797Z',
+    user: '17',
+    content: 'Et voluptatibus officiis praesentium nam molestias deleniti sapiente consectetur.'
+  },
+  {
+    id: '40',
+    createdAt: '2019-11-03T18:42:35.123Z',
+    user: '29',
+    content: 'Quis repellendus aut voluptatum quas.'
+  },
+  {
+    id: '41',
+    createdAt: '2019-11-03T08:32:23.307Z',
+    user: '18',
+    content: 'Similique est accusantium iusto non.'
+  },
+  {
+    id: '42',
+    createdAt: '2019-11-03T02:37:34.033Z',
+    user: '3',
+    content: 'Doloribus alias dolorem corrupti nulla ut cum laudantium omnis laudantium.'
+  },
+  {
+    id: '43',
+    createdAt: '2019-11-03T22:31:16.339Z',
+    user: '12',
+    content: 'Iusto aut optio.'
+  },
+  {
+    id: '44',
+    createdAt: '2019-11-03T12:07:40.633Z',
+    user: '15',
+    content: 'Et est dolorem.'
+  },
+  {
+    id: '45',
+    createdAt: '2019-11-03T22:49:08.949Z',
+    user: '12',
+    content: 'Cumque deserunt eos sed modi ut quos et sint.'
+  },
+  {
+    id: '46',
+    createdAt: '2019-11-03T15:23:57.733Z',
+    user: '13',
+    content: 'Voluptatem tempore modi placeat nihil nemo sunt.'
+  },
+  {
+    id: '47',
+    createdAt: '2019-11-03T10:07:51.406Z',
+    user: '21',
+    content: 'Asperiores atque ut voluptas et.'
+  },
+  {
+    id: '48',
+    createdAt: '2019-11-03T03:40:32.971Z',
+    user: '22',
+    content: 'Facilis officia consequuntur.'
+  },
+  {
+    id: '49',
+    createdAt: '2019-11-03T09:32:59.326Z',
+    user: '25',
+    content: 'Ut velit est repudiandae id nihil qui alias rem.'
+  },
+  {
+    id: '50',
+    createdAt: '2019-11-03T08:00:15.282Z',
+    user: '29',
+    content: 'Sunt et autem quam quas fugiat.'
+  },
+  {
+    id: '51',
+    createdAt: '2019-11-03T08:20:16.846Z',
+    user: '9',
+    content: 'Consectetur sit qui rem.'
+  },
+  {
+    id: '52',
+    createdAt: '2019-11-03T07:31:39.769Z',
+    user: '27',
+    content: 'Adipisci doloribus officiis autem mollitia dolorem.'
+  },
+  {
+    id: '53',
+    createdAt: '2019-11-03T14:47:25.201Z',
+    user: '19',
+    content: 'Qui nihil voluptatem est quidem ea.'
+  },
+  {
+    id: '54',
+    createdAt: '2019-11-03T19:44:38.037Z',
+    user: '23',
+    content: 'Voluptas voluptatibus aperiam.'
+  },
+  {
+    id: '55',
+    createdAt: '2019-11-04T00:24:38.546Z',
+    user: '11',
+    content: 'Laboriosam non eos et voluptatem quisquam officiis.'
+  },
+  {
+    id: '56',
+    createdAt: '2019-11-03T18:44:43.284Z',
+    user: '11',
+    content: 'Aut enim nemo nemo itaque sunt.'
+  },
+  {
+    id: '57',
+    createdAt: '2019-11-03T15:50:10.361Z',
+    user: '19',
+    content: 'Maiores et beatae eum nihil aut dolorem quas.'
+  },
+  {
+    id: '58',
+    createdAt: '2019-11-03T23:11:08.163Z',
+    user: '14',
+    content: 'Sequi quaerat sequi explicabo illum minus qui ut.'
+  },
+  {
+    id: '59',
+    createdAt: '2019-11-03T11:12:26.651Z',
+    user: '26',
+    content: 'Qui non laudantium veritatis et eum dolores qui.'
+  },
+  {
+    id: '60',
+    createdAt: '2019-11-03T14:09:24.276Z',
+    user: '9',
+    content: 'Repellat dicta eos beatae voluptas.'
+  },
+  {
+    id: '61',
+    createdAt: '2019-11-03T05:41:22.961Z',
+    user: '24',
+    content: 'Ducimus aliquid dolor dicta libero adipisci.'
+  },
+  {
+    id: '62',
+    createdAt: '2019-11-03T22:43:44.954Z',
+    user: '8',
+    content: 'Beatae incidunt est quia excepturi qui.'
+  },
+  {
+    id: '63',
+    createdAt: '2019-11-03T02:44:48.940Z',
+    user: '30',
+    content: 'Voluptas aut iure laudantium.'
+  },
+  {
+    id: '64',
+    createdAt: '2019-11-03T03:33:47.720Z',
+    user: '10',
+    content: 'Amet et laudantium.'
+  },
+  {
+    id: '65',
+    createdAt: '2019-11-03T06:48:15.000Z',
+    user: '4',
+    content: 'Facilis excepturi qui et facere quae.'
+  },
+  {
+    id: '66',
+    createdAt: '2019-11-03T07:17:07.424Z',
+    user: '24',
+    content: 'Enim consectetur voluptas est.'
+  },
+  {
+    id: '67',
+    createdAt: '2019-11-03T15:27:03.653Z',
+    user: '4',
+    content: 'Rerum et eos.'
+  },
+  {
+    id: '68',
+    createdAt: '2019-11-03T18:34:07.225Z',
+    user: '6',
+    content: 'Possimus quod omnis quaerat sit placeat.'
+  },
+  {
+    id: '69',
+    createdAt: '2019-11-03T09:57:01.494Z',
+    user: '12',
+    content: 'Delectus nam commodi omnis quia nostrum.'
+  },
+  {
+    id: '70',
+    createdAt: '2019-11-03T04:07:16.910Z',
+    user: '14',
+    content: 'Animi voluptatibus omnis recusandae et odio qui ducimus sit placeat.'
+  },
+  {
+    id: '71',
+    createdAt: '2019-11-03T06:08:55.722Z',
+    user: '22',
+    content: 'Ut quae ut natus occaecati.'
+  },
+  {
+    id: '72',
+    createdAt: '2019-11-03T05:17:31.261Z',
+    user: '23',
+    content: 'Doloribus ut rerum ipsa et assumenda placeat maiores.'
+  },
+  {
+    id: '73',
+    createdAt: '2019-11-03T02:18:43.203Z',
+    user: '5',
+    content: 'Consectetur et eos.'
+  },
+  {
+    id: '74',
+    createdAt: '2019-11-03T08:24:05.956Z',
+    user: '1',
+    content: 'Cum incidunt maxime quisquam fugit magni et.'
+  },
+  {
+    id: '75',
+    createdAt: '2019-11-03T19:43:58.747Z',
+    user: '7',
+    content: 'Totam et cumque.'
+  },
+  {
+    id: '76',
+    createdAt: '2019-11-03T22:39:13.436Z',
+    user: '26',
+    content: 'Nesciunt corrupti quasi unde perspiciatis architecto corrupti ut odit.'
+  },
+  {
+    id: '77',
+    createdAt: '2019-11-03T18:17:34.321Z',
+    user: '16',
+    content: 'Praesentium delectus nihil nesciunt dignissimos voluptatem delectus expedita.'
+  },
+  {
+    id: '78',
+    createdAt: '2019-11-03T05:16:18.311Z',
+    user: '4',
+    content: 'Nihil eius esse laborum et tenetur assumenda qui accusantium vero.'
+  },
+  {
+    id: '79',
+    createdAt: '2019-11-03T14:28:10.457Z',
+    user: '30',
+    content: 'Ut tempore et ut rerum fugiat sed.'
+  },
+  {
+    id: '80',
+    createdAt: '2019-11-03T16:32:42.888Z',
+    user: '26',
+    content: 'Occaecati est doloremque quas aut rerum sit.'
+  },
+  {
+    id: '81',
+    createdAt: '2019-11-03T15:29:37.762Z',
+    user: '9',
+    content: 'Placeat facere quisquam consequatur aliquam.'
+  },
+  {
+    id: '82',
+    createdAt: '2019-11-03T07:01:52.590Z',
+    user: '4',
+    content: 'Consequatur distinctio et assumenda eveniet qui atque sed sequi sit.'
+  },
+  {
+    id: '83',
+    createdAt: '2019-11-03T02:38:58.422Z',
+    user: '27',
+    content: 'Eveniet sint est animi ratione ab sapiente error.'
+  },
+  {
+    id: '84',
+    createdAt: '2019-11-03T22:40:12.370Z',
+    user: '25',
+    content: 'Sequi quam dolor.'
+  },
+  {
+    id: '85',
+    createdAt: '2019-11-03T12:18:52.714Z',
+    user: '19',
+    content: 'Quas est amet dolores perferendis repellendus qui eum.'
+  },
+  {
+    id: '86',
+    createdAt: '2019-11-03T14:48:47.147Z',
+    user: '11',
+    content: 'Tenetur eum recusandae quo quia vel.'
+  },
+  {
+    id: '87',
+    createdAt: '2019-11-03T09:31:30.832Z',
+    user: '23',
+    content: 'In a odio eaque iusto amet fuga maxime.'
+  },
+  {
+    id: '88',
+    createdAt: '2019-11-03T16:12:44.224Z',
+    user: '26',
+    content: 'Libero optio et beatae omnis ut.'
+  },
+  {
+    id: '89',
+    createdAt: '2019-11-03T11:02:25.967Z',
+    user: '25',
+    content: 'Quos ut vel illum ut qui nobis cupiditate.'
+  },
+  {
+    id: '90',
+    createdAt: '2019-11-03T12:56:07.572Z',
+    user: '20',
+    content: 'Aliquid similique laborum.'
+  },
+  {
+    id: '91',
+    createdAt: '2019-11-03T13:33:20.743Z',
+    user: '28',
+    content: 'Quae illo rerum quasi neque itaque nihil.'
+  },
+  {
+    id: '92',
+    createdAt: '2019-11-03T20:43:00.505Z',
+    user: '29',
+    content: 'Et enim sunt modi.'
+  },
+  {
+    id: '93',
+    createdAt: '2019-11-03T03:11:02.619Z',
+    user: '16',
+    content: 'Voluptatibus aspernatur suscipit magnam officia eligendi.'
+  },
+  {
+    id: '94',
+    createdAt: '2019-11-03T14:14:41.812Z',
+    user: '22',
+    content: 'Culpa ex delectus esse aliquid.'
+  },
+  {
+    id: '95',
+    createdAt: '2019-11-03T10:47:59.605Z',
+    user: '7',
+    content: 'Laborum doloremque et a accusamus quibusdam illum nemo.'
+  },
+  {
+    id: '96',
+    createdAt: '2019-11-03T05:22:41.964Z',
+    user: '15',
+    content: 'Dolor natus nobis asperiores quia molestias dolor.'
+  },
+  {
+    id: '97',
+    createdAt: '2019-11-03T04:41:58.494Z',
+    user: '14',
+    content: 'Temporibus possimus aperiam neque.'
+  },
+  {
+    id: '98',
+    createdAt: '2019-11-03T13:03:42.192Z',
+    user: '23',
+    content: 'Est omnis nisi voluptas.'
+  },
+  {
+    id: '99',
+    createdAt: '2019-11-03T21:51:38.435Z',
+    user: '4',
+    content: 'Ullam excepturi aperiam quis architecto aliquid at officia dicta ratione.'
+  },
+  {
+    id: '100',
+    createdAt: '2019-11-04T00:44:03.018Z',
+    user: '20',
+    content: 'Fugiat natus consequatur dolorem molestiae iste.'
+  }
+]
+
+export default messages
+
+/* messages generated using faker: https://github.com/marak/Faker.js/ */
